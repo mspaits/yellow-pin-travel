6edca64100101abe936a5a3587ecf5864cdea87f
mspaits <mspaits@gmail.com>
2026-05-09 15:10:03 -0400

    Working pages, routes, and links

--- app.py
+++ app.py
@@ -12,5 +12,47 @@ def index():
     return render_template('index.html')
 
 
+@app.route("/about")
+def about():
+    """About page"""
+
+    return render_template('about.html')
+
+
+@app.route("/services")
+def services():
+    """Services page"""
+
+    return render_template('services.html')
+
+
+@app.route("/contact")
+def contact():
+    """Contact page"""
+
+    return render_template('contact.html')
+
+
+@app.route("/privacy")
+def privacy():
+    """Privacy page"""
+
+    return render_template('privacy.html')
+
+
+@app.route("/terms")
+def terms():
+    """Terms and conditions page"""
+
+    return render_template('terms.html')
+
+
+@app.route("/accessibility")
+def accessability():
+    """Accessibility page"""
+
+    return render_template('accessibility.html')
+
+
 if __name__ == "__main__":
     app.run(debug=True)
