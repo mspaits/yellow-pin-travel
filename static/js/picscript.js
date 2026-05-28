9b57bfdaa08346478865afff9a1cc2915ff90026
mspaits <mspaits@gmail.com>
2026-05-27 20:18:16 -0400

    Added Google tracking JS in header for ad campaign

--- static/js/picscript.js
+++ static/js/picscript.js
@@ -1,12 +1,10 @@
 fetch('/picsapi')
     .then(response => response.json())
     .then(data => {
-        console.log(data);
 
         const randInt = Math.floor(Math.random() * data.length);
         let image = data[randInt];
 
         const picTag = document.getElementById('rotatingImage');
-        console.log(picTag);
         picTag.src = image;
 });
