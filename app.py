3812d9b822351750c2845023171f83fb3dfa70bd
mspaits <mspaits@gmail.com>
2026-06-08 19:56:25 -0400

    Added gmail authentication and send message functions

--- app.py
+++ app.py
@@ -4,7 +4,97 @@ import os
 import json
 from flask import Flask, render_template, send_from_directory
 
+import base64
+from email.message import EmailMessage
+import google.auth
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+from pathlib import Path
+
+
 app = Flask(__name__)
+
+SCOPES = ["https://www.googleapis.com/auth/gmail.send"]
+
+
+# def main():
+#    get_email_creds()
+#    gmail_send_message_jen()
+
+
+def get_email_creds():
+    """Gets gmail API authentication token."""
+
+    # Identify file paths
+    creds_path = Path(".") / "secrets" / "credentials.json"
+    token_path = Path(".") / "secrets" / "token.json"
+
+    creds = None
+
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists(token_path):
+        creds = Credentials.from_authorized_user_file(token_path, SCOPES)
+
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                creds_path, SCOPES
+            )
+            creds = flow.run_local_server(port=0)
+
+        # Save the credentials for the next run
+        with open(token_path, "w") as token:
+            token.write(creds.to_json())
+
+    return creds
+
+
+def gmail_send_message_jen():
+    """Create and send an email message
+    Print the returned  message id
+    Returns: Message object, including message id
+
+    Load pre-authorized user credentials from the environment.
+    TODO(developer) - See https://developers.google.com/identity
+    for guides on implementing OAuth2 for the application.
+    """
+    creds = get_email_creds()
+
+    try:
+        service = build("gmail", "v1", credentials=creds)
+        message = EmailMessage()
+
+        message.set_content("This is automated draft mail")
+
+        message["To"] = "mspaits@gmail.com"
+        message["From"] = "mspaitsdev@gmail.com"
+        message["Subject"] = "Test from YPT"
+
+        # encoded message
+        encoded_message = base64.urlsafe_b64encode(message.as_bytes()).decode()
+
+        create_message = {"raw": encoded_message}
+        # pylint: disable=E1101
+        send_message = (
+            service.users()
+            .messages()
+            .send(userId="me", body=create_message)
+            .execute()
+        )
+        print(f'Message Id: {send_message["id"]}')
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+        send_message = None
+    return send_message
 
 
 @app.route("/")
@@ -88,3 +178,4 @@ def picsapi():
 
 if __name__ == "__main__":
     app.run(debug=True)
+    # main()
